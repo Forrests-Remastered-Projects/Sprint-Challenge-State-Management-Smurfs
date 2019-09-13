e1f0e03c4c630f9e75913eb59c051b60dc7bd672
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-09-13 08:21:09 -0700

    Downloaded dependencies and coded index.js as well as set up components

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />{" "}
+  </Provider>,
+  document.getElementById("root")
+);
